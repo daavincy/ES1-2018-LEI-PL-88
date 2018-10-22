379f7e887792a9241baf44ac4b953f00f585767d
afcma11 <afcma11@iscte-iul.pt>
2018-10-22 15:50:23 +0100

    Alterções no FacebookConetion Criação de estretura da classe SendMail e respectiva GUI~ [Erro] Falta importar a as bibliotecas no Send Email

---
+++ src/BDA/es/projecto/email/SendMail.java
@@ -0,0 +1,49 @@
+package es.projecto.email;
+
+import java.util.Properties;
+
+public class SendMail {
+
+	public static void main(String[] args) {
+		
+	      String to = "a@gmail.com"; //Variavel Y
+
+	      String from = "web@gmail.com"; //Variavel X
+
+	      String host = "localhost";
+
+	      // Get system properties
+	      Properties properties = System.getProperties();
+
+	      // Setup mail server
+	      properties.setProperty("mail.smtp.host", host);
+
+	      // Get the default Session object.
+	      Session session = Session.getDefaultInstance(properties);
+
+	      try {
+	         // Create a default MimeMessage object.
+	         MimeMessage message = new MimeMessage(session);
+
+	         // Set From: header field of the header.
+	         message.setFrom(new InternetAddress(from));
+
+	         // Set To: header field of the header.
+	         message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+	         // Set Subject: header field
+	         message.setSubject("This is the Subject Line!");
+
+	         // Now set the actual message
+	         message.setText("This is actual message");
+
+	         // Send message
+	         Transport.send(message);
+	         System.out.println("Sent message successfully....");
+	      } catch (MessagingException mex) {
+	         mex.printStackTrace();
+	      }
+
+	}
+
+}
--- src/BDA/es/projecto/facebook/conection/FacebookConection.java
+++ src/BDA/es/projecto/facebook/conection/FacebookConection.java
@@ -13,7 +13,6 @@ public class FacebookConection {
 	
 	private static String accessToken="EAAElkZB1PPCABAJqd07ZAZB1gRA6zpJBnBXAvaTFw5qoa4zkdRK7m2SsApCVWhFvLdK4Y14jQcXGuCoJnizGDUCvDJHPzxfQXD0n7FVqdALG6K3XdPiow83TVJ1zb618TgLl78w34lC3Euuz7ZBuNfZCsDIFXOjujXzw6LySLLaHZAZC3REKQwL";
 
-
 	public FacebookConection() {
 	}
 	
@@ -50,6 +49,11 @@ public void getUserTimelinePosts() {
 		
 	}
 
+
+public void getPostsIscte() {
+	
+	
+}
 	public static void main(String[] args) {
 		
 		FacebookConection fc = new FacebookConection();
