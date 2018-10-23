881f76e1112f051f08c62107f4ec2af099c42f31
Cunha <Boda@192.168.1.96>
2018-10-23 13:31:06 +0100

    Faltam as API indicadas no chat.

---
+++ SendMail.java
@@ -0,0 +1,53 @@
+package mail;
+
+import javax.mail.*;
+import javax.activation.*;
+import javax.mail.internet.*;
+
+import java.util.*;
+
+
+public class SendMail {
+	
+	public static void main(String[] args) {
+		try {
+			String host= "smtp.gmail.com";
+			String user= "jpfabc@gmail.com";
+			String pass= "jpfabc123";
+			String to= "jpfabc@live.com.pt";
+			String from= "jpfabc@gmail.com";
+			String subject= "test";
+			String text= "test received";
+			
+			Properties props =System.getProperties();
+			
+			props.put("mail.smtp.starttls.enable", "true");
+			props.put("mail.smtp.host", host);
+			props.put("mail.smtp.port", "587");
+			props.put("mail.smtp.auth", "true");
+			props.put("mail.smtp.strattls.required", "true");
+			
+			//java.security.Security.addProvider(new com.sun.net.ssl.internal.ssl.Provider());
+			Session mailSession=Session.getDefaultInstance(props,null);
+			Message msg = new MimeMessage(mailSession);
+			msg.setFrom(new InternetAddress(from));
+			InternetAddress address= (new InternetAddress(to));
+			msg.setRecipient(Message.RecipientType.TO, address);
+			msg.setSubject(subject);
+			msg.setSentDate(new Date());
+			msg.setText(text);
+			
+			Transport transport=mailSession.getTransport("smtp");
+			transport.connect(host,user,pass);
+			transport.sendMessage(msg, msg.getAllRecipients());
+			transport.close();
+			System.out.println("message sent");
+		}catch(Exception ex)
+		{
+			System.out.println(ex);
+			
+		}
+	}
+		
+	
+} 
