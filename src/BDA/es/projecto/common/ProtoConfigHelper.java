0ae676bf3513953fb850fd11f5a044178d65530e
Elvino Monteiro <a24227@iscte.pt>
2018-11-19 21:29:51 +0000

    Alterado o newsHeaders para conter o subject
    E alterado o produtor de configs

--- src/BDA/es/projecto/common/ProtoConfigHelper.java
+++ src/BDA/es/projecto/common/ProtoConfigHelper.java
@@ -1,11 +1,10 @@
 package es.projecto.common;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
@@ -15,55 +14,62 @@ import es.projecto.hmi.pojos.EmailConfigs;
 import es.projecto.hmi.pojos.FacebookConfigs;
 import es.projecto.hmi.pojos.TwitterConfigs;
 
+
 public class ProtoConfigHelper {
+//	private static final Logger log = Log
 
 	private ObjectMapper objectMapper;
 	private static ProtoConfigHelper instance;
-	
+	private final String CONFIG_FILE;
+
 	public static ProtoConfigHelper getInstance() {
-		if(instance==null)
-			instance=new ProtoConfigHelper();
+		if (instance == null)
+			instance = new ProtoConfigHelper(null);
 		return instance;
 	}
 
-	public ProtoConfigHelper() {
+	ProtoConfigHelper(String filename) {
+		if(filename==null)
+			filename ="configs.xml";
+		
+		CONFIG_FILE=filename;
 		objectMapper = new XmlMapper();
 		objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
 	}
-	
-	void saveConfigurations() {
-		Configurations teste = new Configurations(new FacebookConfigs("access_token", "client_id", "app_id"),new TwitterConfigs("consumer_key", "consumer_secret", "access_token", "access_token_secret"), new EmailConfigs("user", "password"));
-		String xml="";
+
+	/**
+	 * Metodo criado para salvar as configurações
+	 * 
+	 * @param configs representa o objecto {@link Configurations} que é usado para passar as configurações para o ficheiro
+	 */
+	public void saveConfigurations(Configurations configs) {
+
+		File output = new File(CONFIG_FILE);
 		try {
-			xml = objectMapper.writeValueAsString(teste);
-		} catch (JsonProcessingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		File output = new File("configs.xml");
-		try {
-			output.createNewFile();
-			System.out.println(output.getAbsolutePath());
+			if (output.exists()) {
+				objectMapper.writeValue(output, configs);
+			} else {
+				output.createNewFile();
+				objectMapper.writeValue(output, configs);
+			}
 		} catch (IOException e) {
-		} 
-		FileOutputStream oFile = null;
-		try {
-			 oFile = new FileOutputStream(output, false);
-		} catch (FileNotFoundException e) {
-		} 
-		try {
-			oFile.write(xml.getBytes("UTF-8"));
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			
 		}
-		
-		System.out.println(xml);
 	}
 	
-	
-public static void main(String[] args) {
-	ProtoConfigHelper.getInstance().saveConfigurations();
-}
+	public Configurations getConfigurations() throws IOException {
+		File output = new File(CONFIG_FILE);
+		if(output.exists()) {
+			return objectMapper.readValue(output, Configurations.class);
+		}
+		
+		return null;
+	}
+
+	public static void main(String[] args) {
+		ProtoConfigHelper.getInstance()
+				.saveConfigurations(new Configurations(new FacebookConfigs("access_token", "client_id", "app_id"),
+						new TwitterConfigs("consumer_key", "consumer_secret", "access_token", "access_token_secret"),
+						new EmailConfigs("user", "password")));
+	}
 }
--- src/BDA/es/projecto/hmi/pojos/NewsHeaders.java
+++ src/BDA/es/projecto/hmi/pojos/NewsHeaders.java
@@ -2,53 +2,84 @@ package es.projecto.hmi.pojos;
 
 import java.util.Date;
 
+import es.projecto.hmi.utils.Constants;
+
 public class NewsHeaders {
 	private long id;
 	private int provider;
+	private String shorttext;
 	private String text;
 	private String source;
 	private Date date;
 	
-	public NewsHeaders(long id, int provider, String text, String source, Date date) {
+
+/**
+ * Construtor para receber as noticias.
+ * @param id {@link Long} identificador unico da mensagem
+ * @param provider uma das constates da class {@link Constants} {@code FACEBOOK_ID, TWITTER_ID ou EMAIL_ID}
+ * @param shorttext titulo curto ou sujeito da mensagem
+ * @param text corpo da mensagem
+ * @param source remetente da mensagem
+ * @param date data de recepção da mensagem
+ */
+	public NewsHeaders(Long id, int provider, String shorttext, String text, String source, Date date) {
 		super();
-		this.id = id;
+		this.id = id==null?0L:id;
 		this.provider = provider;
+		this.shorttext = shorttext;
 		this.text = text;
 		this.source = source;
 		this.date = date;
 	}
 
-	/**
-	 * @return the id
-	 */
-	public long getId() {
-		return id;
-	}
 
-	/**
-	 * @return the provider
-	 */
-	public int getProvider() {
-		return provider;
-	}
+/**
+ * @return the id
+ */
+public long getId() {
+	return id;
+}
 
-	/**
-	 * @return the text
-	 */
-	public String getText() {
-		return text;
-	}
 
-	/**
-	 * @return the source
-	 */
-	public String getSource() {
-		return source;
-	}
+/**
+ * @return the provider
+ */
+public int getProvider() {
+	return provider;
+}
 
-	public Date getDate() {
-		return date;
-	}
+
+/**
+ * @return the shorttext
+ */
+public String getShorttext() {
+	return shorttext;
+}
+
+
+/**
+ * @return the text
+ */
+public String getText() {
+	return text;
+}
+
+
+/**
+ * @return the source
+ */
+public String getSource() {
+	return source;
+}
+
+
+/**
+ * @return the date
+ */
+public Date getDate() {
+	return date;
+}
+	
 	
 	
 
